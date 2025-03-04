1b3d33eccb5b72a80c86051eb18ab1bbc2882b87
Josh <joshrieltec@gmail.com>
2025-03-04 11:37:49 -0600

    Analisis de codigo sensor HSTS

--- Codigos originales/sensor_HSTS016L_og_code.ino
+++ Codigos originales/sensor_HSTS016L_og_code.ino
@@ -1,65 +1,81 @@
 // Fuente:     https://poweruc.com/faq/how-to-read-current-sensor-with-arduino
 
-// Which pin to measure Current Value (A0 is reserved for LCD Display Shield Button function)
-int currentAnalogInputPin = A1;  
-// Which pin to calibrate offset middle value
-int calibrationPin = A2; 
-// Key in value to manually offset the initial value
-float manualOffset = 0.00;
-// If using “Hall-Effect” Current Transformer, key in value using this formula: mVperAmp = maximum voltage range (in milli volt) / current rating of CT
-float mVperAmpValue = 12.5;        
-// Analog input pin maximum supply voltage, Arduino Uno or Mega is 5000mV while Arduino Nano or Node MCU is 3300mV
-float supplyVoltage = 5000;
-/* to read the value of a sample for offset purpose later */
+
+// --------------DEFINICION DE PINES Y CONSTANTES-------------------------------------
+int currentAnalogInputPin = A1;   // pin para medir corriente (A0 is reserved for LCD Display Shield Button function)
+int calibrationPin = A2;          //  pin para calibrar offset middle value
+float manualOffset = 0.00;        // valor para cambio manual del offset the initial value
+float mVperAmpValue = 12.5;       // If using “Hall-Effect” Current Transformer, key in value using this formula: mVperAmp = maximum voltage range (in milli volt) / current rating of CT
+float supplyVoltage = 3300;  // Yo voy a usar NANO entonces es 3300mV
+// maximum supply voltage, Arduino Uno or Mega is 5000mV while Arduino Nano or Node MCU is 3300mV
+
+
+
+
+
+// ------------------VARIABLES PARA EL PROCESAMIENTO DE SENALES------------------------------------------
+//Estas variables se utilizan para almacenar muestras de corriente, calcular promedios y determinar el valor RMS de la corriente medida.
+
 float offsetSampleRead = 0;        
- /* to read the value of a sample including currentOffset1 value*/
 float currentSampleRead  = 0;     
-  /* to count time for each sample. Technically 1 milli second 1 sample is taken */
 float currentLastSample  = 0;     
-  /* accumulation of sample readings */
 float currentSampleSum   = 0;     
-  /* to count number of sample. */
 float currentSampleCount = 0;     
-    /* to calculate the average value from all samples, in analog values*/
 float currentMean ;            
-  /* square roof of currentMean, in analog values */
 float RMSCurrentMean ;             
-   /* the final RMS current reading*/
 float FinalRMSCurrent ;           
-void setup()                                 /*codes to run once */
+
+
+
+
+
+
+// -------------------CONFIGURACION----------------------------------------------
+void setup()                                 // codes to run once 
 {
-        Serial.begin(9600);                 /* to display readings in Serial Monitor at 9600 baud rates */
+        Serial.begin(9600);                 // Serial Monitor at 9600 baud rates
 }
+
+
+
+
+
+
+
+//----------------------------  ADQUIRIR MUESTRAS  -----------------------------------------------------
 void loop()
 {
-        /* 1- AC & DC Current Measurement */
-        if(micros() >= currentLastSample + 200)                     /* every 0.2 milli second taking 1 reading */
+        if(micros() >= currentLastSample + 200)              // toma una lectura cada 0.2ms, son 5000 muestras por segundo
           {
-           /* read the sample value including offset value*/
-           currentSampleRead = analogRead(currentAnalogInputPin)-analogRead(calibrationPin); 
-          /* accumulate total analog values for each sample readings*/
-           currentSampleSum = currentSampleSum + sq(currentSampleRead) ;
-           /* to count and move on to the next following count */
-           currentSampleCount = currentSampleCount + 1; 
-           /* to reset the time again so that next cycle can start again*/
-           currentLastSample = micros(); 
+           currentSampleRead = analogRead(currentAnalogInputPin)-analogRead(calibrationPin); // Se lee el valor del sensor en A1 y se le resta la referencia en A2 para calibrar el offset
+           currentSampleSum = currentSampleSum + sq(currentSampleRead) ;                     //Se acumulan los valores al cuadrado (sq(x)) para luego calcular la media cuadrática.
+           currentSampleCount = currentSampleCount + 1;                                      // incrementa el conteo de muestras
+           currentLastSample = micros();                                                     // permite iniciar el siguiente conteo de ciclos
           }
-        /* after 4000 count or 800 milli seconds (0.8 second), do this following codes*/
-        if(currentSampleCount == 4000) 
+
+
+        // --------------- PROCESAR DATOS CADA 4000 MUESTRAS O 0.8s --------------------------------
+        if(currentSampleCount == 4000)         
           {
-            /* average accumulated analog values*/
-            currentMean = currentSampleSum/currentSampleCount;
-             /* square root of the average value*/
-            RMSCurrentMean = sqrt(currentMean);
-             /* calculate the final RMS current*/
-            FinalRMSCurrent = (((RMSCurrentMean /1023) *supplyVoltage) /mVperAmpValue)- manualOffset;
-              /* if the current detected is less than or up to 1%, set current value to 0A*/
+            currentMean = currentSampleSum/currentSampleCount;                
+            RMSCurrentMean = sqrt(currentMean);                //  Se calcula la media de los valores al cuadrado y luego se obtiene su raíz cuadrada (RMS).
+            FinalRMSCurrent = (((RMSCurrentMean /1023) *supplyVoltage) /mVperAmpValue)- manualOffset;               /* calculate the final RMS current*/
+            // Se convierte la lectura del ADC (0-1023) a voltaje.
+            // Se divide por mVperAmpValue para obtener la corriente en Amperios.
+            // Se aplica manualOffset para corregir cualquier error de calibración.
+
+
+            // Si la corriente medida es menor al 1% de la capacidad del sensor, se asume que es 0A 
             if(FinalRMSCurrent <= (625/mVperAmpValue/100))
             { FinalRMSCurrent =0; }
-            Serial.print(” The Current RMS value is: “);
+            
+            
+            
+            // ------------------------ MOSTRAR RESULTADOS ----------------------------------------------
+            Serial.print(” La corriente RMS es: “);      // NOTA: LAS COMILLAS PUEDEN ESTAR MAL ESCRITAS
             Serial.print(FinalRMSCurrent,decimalPrecision);
             Serial.println(” A “);
-            currentSampleSum =0;                                 /* to reset accumulate sample values for the next cycle */
-            currentSampleCount=0;                                 /* to reset number of sample for the next cycle */
+            currentSampleSum =0;        // Se reinician los valores acumulados para el siguiente ciclo de medicion
+            currentSampleCount=0;                               
           }
 }
